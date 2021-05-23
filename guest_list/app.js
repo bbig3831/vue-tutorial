a182d3fa8ebbdd94f0e758275971398313adda63
bbig3831 <beb601@mail.harvard.edu>
2021-05-23 19:02:25 -0400

    Finish section on Vue intro

--- guest_list/app.js
+++ guest_list/app.js
@@ -4,7 +4,23 @@ new Vue({
     event: {
       eventDate: 'August 14th - 16th',
       eventTitle: 'Summer Festival!',
-      eventDescription: "It's back! This years summer festival will be in the beautiful countryside featuring our best line up ever!"
+      eventDescription: "It's back! This years summer festival will be in the beautiful countryside featuring our best line up ever!",
+      signupText: 'Add your name to the guest list <em>exclusive</em> offers:',
+    },
+    newNameText: '',
+    guestName: ['James', 'Sam', 'Chris'],
+    formSubmitClass: "",
+    appStyles: {
+      marginTop: '25px'
+    }
+  },
+  methods: {
+    formSubmitted: function() {
+      if(this.newNameText.length > 0) {
+        this.guestName.push(this.newNameText)
+        this.newNameText = ''
+        this.formSubmitClass = "submitted"
+      }
     }
   }
 });
